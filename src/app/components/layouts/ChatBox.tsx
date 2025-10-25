7f6b3cdc7c44db1b13aa68ccc76992214f906983
Jacky <tdhungit@gmail.com>
2025-10-25 08:22:50 +0700

    perf(ChatBox): lazy load chat module to improve initial load time

--- src/app/components/layouts/ChatBox.tsx
+++ src/app/components/layouts/ChatBox.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import ApiService from '@/service/ApiService';
-import { createChat } from '@n8n/chat';
 import '@n8n/chat/style.css';
 import { useEffect } from 'react';
 
 export default function ChatBox() {
   useEffect(() => {
     ApiService.request('GET', '/customers/contact/chatbox-config')
-      .then((res) => {
+      .then(async (res) => {
         if (res?.n8nCRMWebhook) {
+          const { createChat } = await import('@n8n/chat');
           createChat({
             webhookUrl: res.n8nCRMWebhook,
           });
